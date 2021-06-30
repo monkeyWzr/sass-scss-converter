83df0fab092b10b365aa8fe7403f9cf6e68cfaa1
monkeyWzr <monkeywzr@gmail.com>
2021-07-01 00:49:40 +0900

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,89 +1,62 @@
-#!/usr/bin/env node
-
 import fs from 'fs'
 import path from 'path'
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
 
 import { convertSassToScss } from "./util/convertSassToScss";
 import { convertScssToSass } from './util/convertScssToSass';
 
-type ToType = 'sass' | 'scss'
-interface Arguments {
-    [x: string]: unknown;
-    source: string,
-    target: string,
-    toType: ToType
+export type ToType = 'sass' | 'scss'
+export interface Arguments {
+  [x: string]: unknown;
+  source: string,
+  target: string,
+  toType: ToType
 }
-const types: ReadonlyArray<ToType> = ['sass', 'scss']
 
-function convert(dir: string, outDir: string) {
-    console.log('outDir: ' + outDir)
-    if(!fs.existsSync(outDir)) fs.mkdirSync(outDir);
+export function convert(dir: string, outDir: string, toType: ToType) {
+  const sourceFileTypeChecker = (toType == 'scss' ? isSassFile : isScssFile)
+  const converter = (toType == 'scss' ? convertSassToScss : convertScssToSass)
+  convertToTarget(dir, outDir, sourceFileTypeChecker, converter)
+}
 
-    fs.readdirSync(dir).forEach(sub => {
-        console.log('sub: ' + sub);
+function convertToTarget(dir: string, outDir: string,
+  sourceFileTypeChecker: (name: string) => boolean,
+  converter: (name: string) => Promise<string>) {
 
-        const subPath = path.resolve(dir, sub);
-        if (fs.statSync(subPath).isFile()) {
-            if (!sourceFileTypeChecker(sub)) return;
+  // console.log('outDir: ' + outDir)
+  if (!fs.existsSync(outDir)) fs.mkdirSync(outDir, { recursive: true });
 
-            console.log('subPath: ' + subPath)
-            const file = fs.readFileSync(subPath)
-            converter(file.toString()).then(scss => {
-                const subOutPath = path.resolve(outDir, sub.substring(0, sub.lastIndexOf('.')) + '.scss');
+  fs.readdirSync(dir).forEach(sub => {
+    // console.log('sub: ' + sub);
 
-                console.log('writing file to: ' + subOutPath)
-                fs.writeFileSync(subOutPath, scss, { flag: 'wx' })
-                console.log('writing file done')
-            }).catch(e => {
-                console.log('error from file: ' + subPath);
-                console.log(e)
-            })
-        }
-        else if (fs.statSync(subPath).isDirectory()) {
-            const subOutDir = path.resolve(outDir, sub)
-            console.log('into subFolder: ' + subPath)
-            convert(subPath, subOutDir)
-        }
-    })
+    const subPath = path.resolve(dir, sub);
+    if (fs.statSync(subPath).isFile()) {
+      if (!sourceFileTypeChecker(sub)) return;
+
+      // console.log('subPath: ' + subPath)
+      const file = fs.readFileSync(subPath)
+      converter(file.toString()).then(scss => {
+        const subOutPath = path.resolve(outDir, sub.substring(0, sub.lastIndexOf('.')) + '.scss');
+
+        // console.log('writing file to: ' + subOutPath)
+        fs.writeFileSync(subOutPath, scss, { flag: 'wx' })
+        // console.log('writing file done')
+      }).catch(e => {
+        console.log('error from file: ' + subPath);
+        console.log(e)
+      })
+    }
+    else if (fs.statSync(subPath).isDirectory()) {
+      const subOutDir = path.resolve(outDir, sub)
+      // console.log('into subFolder: ' + subPath)
+      convertToTarget(subPath, subOutDir, sourceFileTypeChecker, converter)
+    }
+  })
 }
 
 function isSassFile(fileName: string) {
-    return path.extname(fileName).toLowerCase() == '.sass'
+  return path.extname(fileName).toLowerCase() == '.sass'
 }
 
 function isScssFile(fileName: string) {
-    return path.extname(fileName).toLowerCase() == '.scss'
+  return path.extname(fileName).toLowerCase() == '.scss'
 }
-
-/**
- * convert between Sass and SCSS
- */
-
-const argv =
-    yargs(hideBin(process.argv))
-    .options({
-        source: { type: 'string', demandOption: true },
-        target: { type: 'string', demandOption: true },
-        toType: { choices: types }
-    })
-    .parseSync();
-
-
-const source: string = argv.source
-const target: string = argv.target
-const toType: ToType = argv.toType || 'scss'
-
-
-const sourceFileTypeChecker = (toType == 'scss' ? isSassFile : isScssFile )
-const converter = (toType == 'scss' ? convertSassToScss : convertScssToSass)
-
-console.log('start')
-console.log('source: ' + source)
-console.log('target: ' + target)
-convert(source, target);
-
-process.on("exit", () => {
-    console.log('end')
-})
